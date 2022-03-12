5dad095ed2b422c1f540f20a5a337e5639c034ef
Htet Paing Kyaw <htetpaingk50@gmail.com>
2022-03-13 03:27:18 +0630

    making animation with cssanimation library

--- vendor/wow/wow.js
+++ vendor/wow/wow.js
@@ -214,7 +214,7 @@
 
       this.defaults = {
         boxClass: 'wow',
-        animateClass: 'animated',
+        animateClass: 'cssanimation',
         offset: 0,
         mobile: true,
         live: true,
