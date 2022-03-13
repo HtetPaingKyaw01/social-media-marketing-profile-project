fc8286035a453f1ab58225b274f863520f47e2c1
Htet Paing Kyaw <htetpaingk50@gmail.com>
2022-03-14 02:56:06 +0630

    making nav-bar to current section ux with waypoints library

--- js/app.js
+++ js/app.js
@@ -16,4 +16,47 @@ $(".navbar-toggler").click(function(){
         $(".menu-icon").removeClass("fa-close").addClass("fa-bars");
     }
 });
-    
+
+var waypoints = $('#home').waypoint(function(direction) {
+    console.log("a"); 
+    $(".nav-link").removeClass("current-section");
+    $(".nav-link[href='#home']").addClass("current-section");
+  }, {
+    offset: '25%'
+});
+
+var waypoints = $('#about').waypoint(function(direction) {
+    console.log("b"); 
+    $(".nav-link").removeClass("current-section");
+    $(".nav-link[href='#about']").addClass("current-section");
+
+  }, {
+    offset: '25%'
+});
+
+var waypoints = $('#services').waypoint(function(direction) {
+    console.log("c"); 
+    $(".nav-link").removeClass("current-section");
+    $(".nav-link[href='#services']").addClass("current-section");
+
+  }, {
+    offset: '25%'
+});
+
+var waypoints = $('#pricing').waypoint(function(direction) {
+    console.log("d"); 
+    $(".nav-link").removeClass("current-section");
+    $(".nav-link[href='#pricing']").addClass("current-section");
+
+  }, {
+    offset: '25%'
+});
+
+var waypoints = $('#contact').waypoint(function(direction) {
+    console.log("e"); 
+    $(".nav-link").removeClass("current-section");
+    $(".nav-link[href='#contact']").addClass("current-section");
+
+  }, {
+    offset: '25%'
+});
